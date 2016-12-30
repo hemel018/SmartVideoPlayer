6d392ee0405254c5cbfb93d706468b6ca7d60aac
Rana Asif Bin Hamid <hemel018@gmail.com>
2016-12-30 22:42:48 +0600

    Introducing VideoPlayer

---
+++ smartvideoplayer/src/main/java/com/quanchi/smartvideoplayer/VideoPlayer/VideoPlayer.java
@@ -0,0 +1,20 @@
+package com.quanchi.smartvideoplayer.VideoPlayer;
+
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+public class VideoPlayer extends RelativeLayout {
+
+    public VideoPlayer(Context context){
+        super(context);
+    }
+
+    public void play(){
+
+    }
+
+    public void pause(){
+        
+    }
+}
